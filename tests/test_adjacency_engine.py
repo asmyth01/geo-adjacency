466e90b33b98355562ca4f80367eac5cf9eeda40
Andrew Smyth <andrews@zillowgroup.com>
2023-11-27 14:35:12 -0800

    Use different test data.

--- tests/test_adjacency_engine.py
+++ tests/test_adjacency_engine.py
@@ -2,9 +2,8 @@ from shapely.wkt import dumps
 
 from geo_adjacency import adjacency
 from geo_adjacency.utils import flatten_list
-from tests.utils import load_test_geoms
 
-source_geoms, target_geoms, obstacle_geoms = load_test_geoms("sample_data")
+from tests.sample_data.wkts import source_geoms, target_geoms, obstacle_geoms
 
 
 def test_flatten_list():
@@ -14,17 +13,91 @@ def test_flatten_list():
 def test_get_adjacency_matrix():
     engine = adjacency.AdjacencyEngine(source_geoms, target_geoms, obstacle_geoms, True, 0.001)
     actual = engine.get_adjacency_dict()
-    expected = {0: [4, 7], 1: [4], 3: [2, 3], 4: [3], 5: [3], 8: [4], 9: [0], 13: [2], 15: [3], 16: [0], 17: [0, 7]}
+    expected = {
+        0: [2, 3],
+        1: [0],
+        2: [0, 1, 2],
+        3: [3, 4, 5],
+        4: [5],
+        5: [6],
+        6: [7]
+    }
     assert actual == expected
+
 
 def test_vor():
     engine = adjacency.AdjacencyEngine(source_geoms, target_geoms, obstacle_geoms, False)
-    expected = [23, 10, 21, 6, 9, 8, 5, 1, 2, 4, 45, 71, 14, 52, 0, 7, 3, 62, 60, 57, 63, 62, 26,
-                64, 47, 27, 26, 92, 95, 94, 91, 92, 13, 76, 96, 97, 77, 73, 13, 93, 98, 70, 66, 93,
-                48, 67, 51, 53, 48, 55, 54, 15, 16, 55, 30, 19, 58, 31, 30, 17, 22, 20, 24, 25, 18,
-                17, 56, 46, 41, 29, 56, 44, 75, 79, 74, 44, 90, 69, 86, 68, 90, 72, 87, 50, 49, 72,
-                11, 33, 37, 32, 11, 80, 82, 81, 78, 80, 85, 89, 88, 84, 85, 28, 12, 43, 40, 28, 61,
-                99, 59, 100, 65, 39, 35, 83, 42, 36, 38]
+    expected = [28,
+ 2,
+ 7,
+ 6,
+ 14,
+ 2,
+ 13,
+ 30,
+ 31,
+ 29,
+ 13,
+ 11,
+ 10,
+ 24,
+ 1,
+ 11,
+ 33,
+ 37,
+ 36,
+ 32,
+ 33,
+ 18,
+ 22,
+ 23,
+ 15,
+ 18,
+ 16,
+ 8,
+ 4,
+ 50,
+ 42,
+ 52,
+ 4,
+ 44,
+ 41,
+ 43,
+ 51,
+ 44,
+ 26,
+ 27,
+ 25,
+ 48,
+ 46,
+ 40,
+ 25,
+ 38,
+ 45,
+ 57,
+ 39,
+ 38,
+ 56,
+ 21,
+ 17,
+ 47,
+ 49,
+ 9,
+ 17,
+ 5,
+ 3,
+ 3,
+ 12,
+ 35,
+ 34,
+ 53,
+ 54,
+ 35,
+ 58,
+ 55,
+ 19,
+ 20,
+ 58]
     actual = list(engine.vor.point_region)
     assert actual == expected
 
@@ -32,11 +105,12 @@ def test_vor():
 def test_calc_segmentation_dist():
     engine = adjacency.AdjacencyEngine(source_geoms, target_geoms, obstacle_geoms, True)
     actual = engine.calc_segmentation_dist()
-    expected = 0.0009346349219844988
+    expected = 1.068225655023215
     assert actual == expected
+
 
 def test_get_feature_from_coord_index():
     engine = adjacency.AdjacencyEngine(source_geoms, target_geoms, obstacle_geoms, False)
     actual = dumps(engine.get_feature_from_coord_index(50).geometry)
-    expected = "POLYGON ((-122.4759872510099967 47.7276072458459026, -122.4759507224080011 47.7262312413106997, -122.4729188484759987 47.7262066694707983, -122.4730649628820061 47.7274598185273007, -122.4759872510099967 47.7276072458459026))"
+    expected = "LINESTRING (21.0000000000000000 2.0000000000000000, 26.0000000000000000 2.0000000000000000)"
     assert actual == expected
